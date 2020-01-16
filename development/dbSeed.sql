ef5a9ffe39745451bf23620b9cc5e256fa9a0327
Arauco <arishel@gmail.com>
2020-01-16 10:57:27 -0800

    copy starting files from CatsApp example

--- development/dbSeed.sql
+++
@@ -1,8 +0,0 @@
-USE`da_burger_db`;
-
-INSERT INTO burgers(name, eaten) VALUES('Classic', true);
-INSERT INTO burgers(name, eaten) VALUES('Impossible Burger', false);
-INSERT INTO burgers(name, eaten) VALUES('Chicken Burger', false);
-INSERT INTO burgers(name, eaten) VALUES('Bean Burger', true);
-
-SELECT * from burgers;
